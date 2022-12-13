b74ae5c5cba2f498013856c2a4fc8765200ef3df
Oscar <oscar.zajac99@gmail.com>
2022-12-13 17:03:37 +0100

    initial commit

---
+++ src/main/java/com/interview/Currencies/CurrenciesApplication.java
@@ -0,0 +1,13 @@
+package com.interview.Currencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrenciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/Currencies/CurrenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.Currencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
